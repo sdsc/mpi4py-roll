f8554dbc76c18e44c2c8d5e0a2c4ce2726250277
Jerry Greenberg <jpg@sdsc.edu>
2014-02-05 13:21:17 -0800

    corrected rpm name

--- src/mpi4py/version.mk
+++ src/mpi4py/version.mk
@@ -1,15 +1,16 @@
 PKGROOT            = /opt/mpi4py
-NAME               = mpi4py
+NAME               = mpi4py_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
 VERSION            = 1.3.1
 RELEASE            = 0
 TARBALL_POSTFIX    = tar.gz
 
 SRC_SUBDIR         = mpi4py
 
-SOURCE_NAME        = $(NAME)
+SOURCE_NAME        = mpi4py
 SOURCE_VERSION     = $(VERSION)
 SOURCE_SUFFIX      = tar.gz
 SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
 SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
 
 TAR_GZ_PKGS           = $(SOURCE_PKG)
+RPM.EXTRAS         = AutoReq:No
